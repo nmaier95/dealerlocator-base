b346e8066351651896f5c06625e7a7fb1e9313da
Niklas Maier <n.maier@syzygy.de>
2020-03-04 10:34:26 +0100

    Cleanup jsdocs comments
    Summary:
    - fix invalid and old comments
    - add typings
    - add class comment to GoogleMap

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import DealerLocator from './partials/DealerLocator';
 import GoogleMap from './partials/GoogleMap';
 import MapsDataService from './partials/MapsDataService';
 import MapsDataSource from './partials/MapsDataSource';
-import MarkerClusterer from './partials/MarkerClusterer';
+import MarkerClusterer from './lib/MarkerClusterer';
 
 export {
 	DealerLocator,
